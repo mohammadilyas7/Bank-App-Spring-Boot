116b783433c9177a66abc2cbda2b2eaab0711d65
ilyas78874@gmail.com <ilyas78874@gmail.com>
2023-09-23 13:20:40 +0530

    userprofile service

---
+++ User-Profile-Service/src/test/java/UserProfileService/UserProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package UserProfileService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
